99a372edfd83ab16a78cd3e93cbed5b01cd3845b
geekman2 <geekmann2@gmail.com>
2016-07-21 10:24:21 -0700

    Finished, I think

--- smartcab/agent.py
+++ smartcab/agent.py
@@ -2,18 +2,44 @@ import random
 from environment import Agent, Environment
 from planner import RoutePlanner
 from simulator import Simulator
+import pandas as pd
+
 
 class LearningAgent(Agent):
     """An agent that learns to drive in the smartcab world."""
 
     def __init__(self, env):
-        super(LearningAgent, self).__init__(env)  # sets self.env = env, state = None, next_waypoint = None, and a default color
+        super(LearningAgent, self).__init__(
+            env)  # sets self.env = env, state = None, next_waypoint = None, and a default color
         self.color = 'red'  # override color
         self.planner = RoutePlanner(self.env, self)  # simple route planner to get next_waypoint
         # TODO: Initialize any additional variables here
+        self.Q_table = {}
+        self.net_reward = 0
+        self.states = set()
+        self.wins = 0
+        self.losses = 0
+
+    def Q(self, state, action):
+        if state in self.Q_table:
+            if action in self.Q_table[state]:
+                return self.Q_table[state][action]
+            else:
+                self.Q_table[state][action] = 1
+                return 1
+        else:
+            self.Q_table[state] = {"left": 1,
+                                   "right": 1,
+                                   "forward": 1,
+                                   None: 1}
+            return 1
 
     def reset(self, destination=None):
         self.planner.route_to(destination)
+        self.net_reward = 0
+
+        pd.DataFrame.from_dict(self.Q_table, orient="index").to_csv("Q.csv")
+
         # TODO: Prepare for a new trip; reset any variables here, if required
 
     def update(self, t):
@@ -21,26 +47,49 @@ class LearningAgent(Agent):
         self.next_waypoint = self.planner.next_waypoint()  # from route planner, also displayed by simulator
         inputs = self.env.sense(self)
         deadline = self.env.get_deadline(self)
+        gamma = 0.0
+        try:
+            epsilon = 1
+        except ZeroDivisionError:
+            epsilon = 0
 
         # TODO: Update state
-        self.state = {"deadline": deadline,
-                      "oncoming": inputs["oncoming"],
-                      "right": inputs["right"],
-                      "left": inputs["left"],
-                      "light": inputs["light"],
-                      "next_waypoint": self.next_waypoint,
-                      "destination": self.destination
-                      }
-        
+        self.state = ["oncoming:{}".format(inputs["oncoming"]),
+                      # inputs["right"],
+                      "left:{}".format(inputs["left"]),
+                      "light:{}".format(inputs["light"]),
+                      "waypoint:{}".format(self.next_waypoint)]
+        self.state = ','.join([str(v) for v in self.state])
+
         # TODO: Select action according to your policy
-        action = random.choice(['forward', 'left', 'right'])
+        if random.random() < epsilon and self.state in self.states:
+            action = max(self.Q_table[self.state], key=self.Q_table[self.state].get)
+        else:
+            action = random.choice(["left", "right", "forward", None])
+            self.states.add(self.state)
 
         # Execute action and get reward
         reward = self.env.act(self, action)
+        self.net_reward += reward
+
+        new_state = ["oncoming:{}".format(inputs["oncoming"]),
+                     # inputs["right"],
+                     "left:{}".format(inputs["left"]),
+                     "light:{}".format(inputs["light"]),
+                     "waypoint:{}".format(self.next_waypoint)]
+        new_state = ','.join(str(v) for v in new_state)
 
         # TODO: Learn policy based on state, action, reward
+        next_rewards = [self.Q(state, act) for state, act in
+                        zip([new_state for x in range(3)], ["left", "right", "forward", None])]
+        if self.state in self.Q_table:
+            self.Q_table[self.state][action] = reward #+ gamma*max(next_rewards)
+        else:
+            self.Q_table[self.state] = {action: reward} #+ gamma*max(next_rewards)}
 
-        print "LearningAgent.update(): deadline = {}, inputs = {}, action = {}, reward = {}".format(deadline, inputs, action, reward)  # [debug]
+
+
+            # print "LearningAgent.update(): deadline = {}, inputs = {}, action = {}, reward = {}".format(deadline,inputs, action, reward)  # [debug]
 
 
 def run():
@@ -49,11 +98,12 @@ def run():
     # Set up environment and agent
     e = Environment()  # create environment (also adds some dummy traffic)
     a = e.create_agent(LearningAgent)  # create agent
-    e.set_primary_agent(a, enforce_deadline=False)  # specify agent to track
+    e.set_primary_agent(a, enforce_deadline=True)  # specify agent to track
     # NOTE: You can set enforce_deadline=False while debugging to allow longer trials
 
     # Now simulate it
-    sim = Simulator(e, update_delay=0.5, display=True)  # create simulator (uses pygame when display=True, if available)
+    sim = Simulator(e, update_delay=0.5,
+                    display=True)  # create simulator (uses pygame when display=True, if available)
     # NOTE: To speed up simulation, reduce update_delay and/or set display=False
 
     sim.run(n_trials=100)  # run for a specified number of trials
